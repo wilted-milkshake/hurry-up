742b017497429b9fb20d73a766e45cb4a1a5d811
Rane Gridley <ranegri3@gmail.com>
2016-04-07 12:28:44 -0700

    Change Backgound Color
    -To test if react-native is working

--- client/index.ios.js
+++ client/index.ios.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#00b37a',
   },
   welcome: {
     fontSize: 20,
