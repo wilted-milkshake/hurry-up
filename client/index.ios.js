948fa402a70da1827e800f01218ac2480c0354c9
Rane Gridley <ranegri3@gmail.com>
2016-04-13 11:13:59 -0700

    Add scrollable views
    -Just Create Event and My events views added

--- client/index.ios.js
+++ client/index.ios.js
@@ -17,7 +17,7 @@ import Form from 'react-native-form';
 
 import {sendEvent, updateLocation} from './helpers/request-helpers';
 
-//import Location from 'react-native-location';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 
 class hurryup extends Component {
 
@@ -49,6 +49,7 @@ class hurryup extends Component {
     updateLocation(origin);
 
     this.watchID = navigator.geolocation.watchPosition((position) => {
+      console.log("WORKING >>>>>>>>>>>");
       var lastPosition = position;
       this.setState({lastPosition});
 
@@ -60,11 +61,11 @@ class hurryup extends Component {
 
       var distanceTraveled = Math.sqrt(Math.pow((initialLatitude - lastLatitude), 2) + Math.pow((initialLongitude - lastLongitude), 2));
 
+      var that = this;
       if (distanceTraveled >= 0.004) { //TODO: set constant for 0.004
-        var that = this;
         updateLocation(this.state.lastPosition.coords, that);
         this.setState({ initialPosition: lastPosition });
-      } 
+      }
     },
     (error) => alert(error.message),
     {enableHighAccuracy: true, timeout: 20000, maximumAge: 60000});
@@ -72,77 +73,84 @@ class hurryup extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Form ref='form'>
-          <Text style={styles.welcome}>
-            Hurry Up!
-          </Text>
+      <View style={styles.parent}>
+      <Text style={styles.welcome}>
+        Hurry Up!
+      </Text>
+      <ScrollableTabView style={{marginTop: 10, }} tabBarBackgroundColor="#499371">
+        <View tabLabel='Create Event' style={styles.container}>
+          <Form ref='form'>
 
-          <Text style={styles.instructions}>
-            Your Event:
-          </Text>
-          <View>
-            <TextInput
-              style={styles.eventName}
-              type='TextInput'
-              name="eventName"
-              placeholder='Cool Party'/>
-          </View>
+            <Text style={styles.instructions}>
+              Your Event:
+            </Text>
+            <View>
+              <TextInput
+                style={styles.eventName}
+                type='TextInput'
+                name="eventName"
+                placeholder='Cool Party'/>
+            </View>
 
-          <Text style={styles.instructions}>
-            Event Location:
-          </Text>
-          <View>
-            <TextInput
-              style={styles.eventName}
-              type='TextInput'
-              name="destination"
-              placeholder='SomePlace'/>
-          </View>
+            <Text style={styles.instructions}>
+              Event Location:
+            </Text>
+            <View>
+              <TextInput
+                style={styles.eventName}
+                type='TextInput'
+                name="destination"
+                placeholder='SomePlace'/>
+            </View>
 
-          <Text style={styles.instructions}>
-            Event Time:
-          </Text>
-          <View>
-            <TextInput
-              style={styles.eventName}
-              type='TextInput'
-              name="eventTime"
-              placeholder='1800'/>
-          </View>
+            <Text style={styles.instructions}>
+              Event Time:
+            </Text>
+            <View>
+              <TextInput
+                style={styles.eventName}
+                type='TextInput'
+                name="eventTime"
+                placeholder='1800'/>
+            </View>
 
-          <Text style={styles.instructions}>
-            How Early Do You Want To Get There:
-          </Text>
-          <View>
-            <TextInput
-              style={styles.eventName}
-              type='TextInput'
-              name="earlyArrival"
-              placeholder='5 min'/>
-          </View>
+            <Text style={styles.instructions}>
+              How Early Do You Want To Get There:
+            </Text>
+            <View>
+              <TextInput
+                style={styles.eventName}
+                type='TextInput'
+                name="earlyArrival"
+                placeholder='5 min'/>
+            </View>
 
-          <Text style={styles.instructions}>
-            How Are You Getting There:
-          </Text>
-          <View>
-            <TextInput
-              style={styles.eventName}
-              type='TextInput'
-              name="mode"
-              placeholder='driving, transit, walking' />
-          </View>
-        </Form>
+            <Text style={styles.instructions}>
+              How Are You Getting There:
+            </Text>
+            <View>
+              <TextInput
+                style={styles.eventName}
+                type='TextInput'
+                name="mode"
+                placeholder='driving, transit, walking' />
+            </View>
+          </Form>
 
-        <TouchableHighlight
-          style={styles.button}
-          onPress={this.buttonClicked.bind(this)}>
-          <View>
-            <Text style={styles.buttonText}>Submit!</Text>
-          </View>
-        </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.buttonClicked.bind(this)}>
+            <View>
+              <Text style={styles.buttonText}>Submit!</Text>
+            </View>
+          </TouchableHighlight>
 
 
+        </View>
+        <View tabLabel='My Events' style={styles.container}>
+          <Text style={styles.welcome}>AWESOME</Text>
+        </View>
+      </ScrollableTabView>
       </View>
     );
   }
@@ -150,6 +158,10 @@ class hurryup extends Component {
 
 
 const styles = StyleSheet.create({
+  parent: {
+    flex: 1,
+    backgroundColor: '#499371'
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -159,7 +171,7 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    marginTop: 40,
   },
   instructions: {
     textAlign: 'center',
