1cc732e0884c1623a5d7497a764d6756ac8cd614
vinithasraja <snellhome1@gmail.com>
2016-04-13 14:14:28 -0700

    added process env  port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 var app = require('./router.js');
 
-console.log('Hurry-up is listening on 8080');
+//console.log('Hurry-up is listening on 8080');
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
-app.listen(8080);
+
