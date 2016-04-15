9959fd3ce4828b5d60e0eaf447ab657b85d495f4
swfree <skye.free@gmail.com>
2016-04-15 16:16:56 -0700

    Update Picker module to be compatible as an export

--- client/views/picker.js
+++ client/views/picker.js
@@ -1,22 +1,41 @@
-import {Component} from 'react-native';
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Image,
+  Dimensions,
+  Animated,
+  PickerIOS
+} from 'react-native';
 
-export default class Picker extends Component {
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+const earlyArrivalTimes = [{time: '5 minutes', value: 300},{time: '10 minutes', value: 600},{time: '15 minutes', value: 900}, {time: '20 minutes', value: 1200}];
+const PickerItemIOS = PickerIOS.Item;
+
+class Picker extends Component {
 
   constructor(props) {
     super(props);
   }
+
   componentDidMount() {
     Animated.timing(this.props.offSet, {
        duration: 300,
        toValue: 50
      }).start();
   }
+
   closeModal() {
     Animated.timing(this.props.offSet, {
        duration: 300,
        toValue: deviceHeight
     }).start(this.props.closeModal);
   }
+
   render() {
     return (
       <Animated.View style={{ transform: [{translateY: this.props.offSet}], backgroundColor: '#D8D8D8', position: 'absolute', width: 410, height: 290, top: 20, left: -10}}>
@@ -26,13 +45,13 @@ export default class Picker extends Component {
             </TouchableHighlight>
           </View>
           <PickerIOS
-          selectedValue={this.props.showtime}
-          onValueChange={(index) => this.props.changeTime(index)}>
-          {Object.keys(showtimes).map((time) => (
+          selectedValue={this.props.earlyArrivalIndex}
+          onValueChange={(index) => this.props.changeEarlyArrival(index)}>
+          {Object.keys(earlyArrivalTimes).map((time) => (
             <PickerItemIOS
               key={time}
               value={time}
-              label={showtimes[time].time}
+              label={earlyArrivalTimes[time].time}
             />
           ))}
         </PickerIOS>
@@ -40,3 +59,25 @@ export default class Picker extends Component {
     );
   }
 };
+
+const styles = StyleSheet.create({
+  closeButtonContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'flex-end', 
+    borderTopColor: '#e2e2e2', 
+    borderTopWidth: 1, 
+    borderBottomColor: '#e2e2e2', 
+    borderBottomWidth:1
+  },
+  closeButton: {
+   paddingRight:10, 
+    paddingTop:10, 
+    paddingBottom:10
+  },
+  closeButtonText: {
+   color: '#027afe'
+  },
+
+});
+
+export default Picker;
