fa953d317b57fbe317ec77a190ddd4405c5073e8
Rane Gridley <ranegri3@gmail.com>
2016-04-16 02:46:54 -0700

    Style Segmented Controller
    -Add additional options for early arrival and mode of transport
    -Add to work so server doesn't crash if error with google api

--- client/views/create-event.js
+++ client/views/create-event.js
@@ -15,10 +15,10 @@ import React, {
 import Picker from './picker';
 import {sendEvent, updateLocation} from '../helpers/request-helpers';
 
-const deviceWidth       = Dimensions.get('window').width;
-const deviceHeight      = Dimensions.get('window').height;
-const earlyArrivalTimes = [{time: '5 minutes', value: '300'},{time: '10 minutes', value: '600'},{time: '15 minutes', value: '900'}, {time: '20 minutes', value: '1200'}];
 const DISTANCE_TO_REFRESH = 0.004;
+const deviceWidth         = Dimensions.get('window').width;
+const deviceHeight        = Dimensions.get('window').height;
+const earlyArrivalTimes   = [{time: '5 minutes', value: '300'},{time: '10 minutes', value: '600'},{time: '15 minutes', value: '900'}, {time: '20 minutes', value: '1200'}, {time: '30 minutes', value: '1800'}, {time: '45 minutes', value: '2700'}, {time: '1 hour', value: '3600'}];
 
 class CreateEvent extends Component {
 
@@ -34,8 +34,8 @@ class CreateEvent extends Component {
       eventTime: '',
       destination: '',
       earlyArrivalIndex: 0,
-      mode: 'not selected',
-      values: ['walk' ,'drive' ,'public transport'],
+      mode: 'Driving',
+      values: ['Driving', 'Walking' , 'Bicycling', 'Transit'],
       offSet: new Animated.Value(deviceHeight),
     };
   }
@@ -59,7 +59,7 @@ class CreateEvent extends Component {
       eventTime: '',
       destination: '',
       earlyArrivalIndex: 0,
-      mode: '',
+  //  mode: 'Driving',  //Until refresh unhighlights previous selected segment
       modal: false,
     });
   }
@@ -73,7 +73,8 @@ class CreateEvent extends Component {
       mode: this.state.mode,
     };
     this.clearForm();
-    var origin    = this.state.initialPosition.coords;
+    var origin   = this.state.initialPosition.coords;
+
     sendEvent(newEvent);
     updateLocation(origin);
 
@@ -102,12 +103,11 @@ class CreateEvent extends Component {
 
   _onChange(event){
     this.setState({
-    selectedIndex: event.nativeEvent.selectedSegmentIndex,
+      selectedIndex: event.nativeEvent.selectedSegmentIndex,
     });
   }
 
   _onValueChange(value) {
-    console.log('value>>>>>',value);
     this.setState({
       mode: value,
     });
@@ -119,7 +119,7 @@ class CreateEvent extends Component {
         <View style={styles.inputsContainer}>
 
           <View style={styles.inputContainer}>
-            <TextInput 
+            <TextInput
               style={[styles.inputFormat, styles.inputStyle]}
               placeholder="Event Name"
               placeholderTextColor="#F5F5F6"
@@ -129,7 +129,7 @@ class CreateEvent extends Component {
           </View>
 
           <View style={styles.inputContainer}>
-            <TextInput 
+            <TextInput
               style={[styles.inputFormat, styles.inputStyle]}
               placeholder="Event Location"
               placeholderTextColor="#F5F5F6"
@@ -139,7 +139,7 @@ class CreateEvent extends Component {
           </View>
 
           <View style={styles.inputContainer}>
-            <TextInput 
+            <TextInput
               style={[styles.inputFormat, styles.inputStyle]}
               placeholder="Event Time"
               placeholderTextColor="#F5F5F6"
@@ -152,15 +152,17 @@ class CreateEvent extends Component {
             <TouchableHighlight style={styles.inputFormat} underlayColor="transparent" onPress={ () => this.setState({modal: true}) }>
               <Text style={styles.inputStyle}>Early Arrival -- {earlyArrivalTimes[this.state.earlyArrivalIndex].time}</Text>
             </TouchableHighlight>
-            { this.state.modal ? <Picker closeModal={() => this.setState({ modal: false })} offSet={this.state.offSet} changeEarlyArrival={this.changeEarlyArrival.bind(this)} earlyArrivalIndex={this.state.earlyArrivalIndex} /> : null } 
+            { this.state.modal ? <Picker closeModal={() => this.setState({ modal: false })} offSet={this.state.offSet} changeEarlyArrival={this.changeEarlyArrival.bind(this)} earlyArrivalIndex={this.state.earlyArrivalIndex} /> : null }
           </View>
 
-          <View style={this.state.modal ? styles.hidden : styles.inputContainer}>
-            <TextInput 
+          <View style={this.state.modal ? styles.hidden : styles.segmentedContainer}>
+            <TextInput
               style={[styles.inputFormat, styles.inputStyle]}
               placeholder="Mode of Transport"
               placeholderTextColor="#F5F5F6"/>
-            <SegmentedControlIOS tintColor="#66ccff"
+              <View style={styles.segmentedSpacing}></View>
+            <SegmentedControlIOS tintColor="#CCC"
+              style={styles.segmented}
               values={this.state.values}
               onChange={this._onChange.bind(this)}
               onValueChange={this._onValueChange.bind(this)}/>
@@ -190,9 +192,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   inputsContainer: {
-    marginTop: 20,
-    marginBottom: 10,
+    marginTop: 25,
+    marginBottom: 15,
+    paddingTop: 20,
     flex: .75
+  },
+  segmentedContainer: {
+    padding: 10,
+    margin: 10,
   },
   inputContainer: {
     padding: 10,
@@ -234,14 +241,16 @@ const styles = StyleSheet.create({
     borderBottomColor: '#F5F5F6',
     borderColor: 'transparent'
   },
-  text: {
-    fontSize: 14,
-    textAlign: 'center',
-    fontWeight: '500',
-    margin: 10,
-  },
   hidden: {
     opacity: 0,
+  },
+  segmented: {
+    height: 40,
+    borderWidth: 2,
+    borderColor: '#CCC',
+  },
+  segmentedSpacing: {
+    height: 20,
   }
 });
 
--- server/worker.js
+++ server/worker.js
@@ -1,5 +1,5 @@
-var request = require('request');
-var API_KEYS = require('./api_keys.js');
+var request    = require('request');
+var API_KEYS   = require('./api_keys.js');
 var TwilioSend = require('./twilio-api-calls.js');
 
 // note: in-memory storage ? write to db?
@@ -10,31 +10,33 @@ var worker = function(event, origin, responseToClient) {
 
 
   // var arrivalTime = event.eventTime (convert to UTC sec) - event.earlyArrival (convert to UTC sec);
-  var arrivalTime = Date.now()/1000; //seconds
-  var currentTime = Date.now()/1000; //seconds
+  var arrivalTime  = Date.now()/1000; //seconds
+  var currentTime  = Date.now()/1000; //seconds
 
-  var originLat = origin.latitude; //37.773972
-  var originLong = origin.longitude; //-122.431297
+  var originLat    = origin.latitude; //37.773972
+  var originLong   = origin.longitude; //-122.431297
   var eventAddress = event.destination; //'1118FolsomStreet,SanFrancisco,CA';
     //split into each field
-  var travelMode = event.mode;   //'driving';
+  var travelMode   = event.mode;   //'driving';
 
   // 1. get API directions time duration
-  var apiRequest = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + originLat + ',' + originLong + '&destination=' + eventAddress +  '&mode=' + travelMode + '&key=' + API_KEYS.googleAPI;
+  var apiRequest   = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + originLat + ',' + originLong + '&destination=' + eventAddress +  '&mode=' + travelMode + '&key=' + API_KEYS.googleAPI;
 
   request(apiRequest, function(err, res, body) {
-    if (err) { throw err; }
     var parsedBody = JSON.parse(body);
-    var duration = parsedBody.routes[0].legs[0].duration.value;
-    var timeoutDuration = (arrivalTime - duration) - currentTime;
-    if (timeoutDuration < 0) {
-      timeoutDuration = 0;
+    if (err || !parsedBody.routes[0]) { console.log('There was an error with Google API', err); }
+    else {
+      var duration = parsedBody.routes[0].legs[0].duration.value;
+      var timeoutDuration = (arrivalTime - duration) - currentTime;
+      if (timeoutDuration < 0) {
+        timeoutDuration = 0;
+      }
+      if (events[event.id]) {
+        clearTimeout(events[event.id]);
+      }
+      //send twilio third parameter ultimately will be ( duration + early arrival to delete when the event starts, now just hardcoded to delete after 5 sec.)
+      events[event.id] = setTimeout(function() { TwilioSend('+19258726914', event, 5000); }, timeoutDuration*1000);
     }
-    if (events[event.id]) {
-      clearTimeout(events[event.id]);
-    }
-    //send twilio third parameter ultimately will be ( duration + early arrival to delete when the event starts, now just hardcoded to delete after 5 sec.)
-    events[event.id] = setTimeout(function() { TwilioSend('+19258726914', event, 5000); }, timeoutDuration*1000);
   });
 };
 
