84d1b9a7328d0f8b1a10403f31347600a0784633
swfree <skye.free@gmail.com>
2016-04-18 13:04:04 -0700

    Parse date from client and set twilioSent in db

--- server/workers/google-api-call.js
+++ server/workers/google-api-call.js
@@ -1,14 +1,31 @@
 var request    = require('request');
 var API_KEYS   = require('../api_keys.js');
 var TwilioSend = require('./twilio-api-call.js');
+var Event      = require('../app/models/event.js');
 
 // note: in-memory storage ? write to db?  ...for clearing timeouts
 var events = {};
 
+var alreadySentTwilio = function(event) {
+  new Event({ id: event.id })
+    .fetch()
+    .then(function(event) {
+      event.set('twilioSent', 'true');
+      event.save()
+        .then(function(updatedEvent) {
+          console.log('Change twilioSent for event: ', updatedEvent);
+        })
+        .catch(function(err) {
+          console.log('Error updated twilioSent for event ', err);
+        });
+      });
+};
+
 var googleWorker = function(event, origin, phoneNumber) {
   // TODO: parse eventtime and earlyarrival to manipulate milliseconds // validate user form entry
   // var arrivalTime = event.eventTime (convert to UTC sec) - event.earlyArrival (convert to UTC sec);
-  var arrivalTime  = Date.now()/1000;     //seconds
+  var arrivalTime  = Date.parse(event.eventTime)/1000 - event.earlyArrival;
+  console.log('>>>>ARRIVAL TIME: ', arrivalTime);
   var currentTime  = Date.now()/1000;     //seconds
 
     //split into each field
@@ -16,7 +33,7 @@ var googleWorker = function(event, origin, phoneNumber) {
   var originLong   = origin.longitude;    //-122.431297
   //'1118FolsomStreet,SanFrancisco,CA' Doesnt actually need spaces removed, but regex practice is nice;
   var eventAddress = event.address.replace(/\s/g, '') + event.city.replace(/\s/g, '') + event.state;
-  var travelMode   = event.mode;          //'driving';
+  var travelMode   = event.mode.toLowerCase();          //'driving';
 
   // Get routes time duration from Google API
   var apiRequest   = 'https://maps.googleapis.com/maps/api/directions/json?' +
@@ -25,11 +42,14 @@ var googleWorker = function(event, origin, phoneNumber) {
     '&mode=' + travelMode +
     '&key=' + API_KEYS.googleAPI;
 
+  console.log("API REQUEST STRING: ", apiRequest);
+
   request(apiRequest, function(err, res, body) {
     var parsedBody = JSON.parse(body);
     if (err || !parsedBody.routes[0]) { console.log('There was an error with Google API', err); }
     else {
       var duration = parsedBody.routes[0].legs[0].duration.value;
+      console.log('>>>>GOOGLE API DURATION: ', duration);
       var timeoutDuration = (arrivalTime - duration) - currentTime;
       if (timeoutDuration < 0) {
         timeoutDuration = 0;
@@ -37,8 +57,12 @@ var googleWorker = function(event, origin, phoneNumber) {
       if (events[event.id]) {
         clearTimeout(events[event.id]);
       }
+      console.log('>>>>TIMEOUT DURATION: ', timeoutDuration);
       //send twilio third parameter ultimately will be ( duration + early arrival) to delete when the event starts, now just hardcoded to delete after 5 sec.)
-      events[event.id] = setTimeout(function() { TwilioSend('+19258726914', event, 5000); }, timeoutDuration*1000);
+      events[event.id] = setTimeout(function() { 
+        TwilioSend(phoneNumber, event, duration + event.earlyArrival); 
+        alreadySentTwilio(event);
+      }, timeoutDuration*1000);
     }
   });
 };
