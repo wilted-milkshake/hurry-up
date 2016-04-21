c7e8318446078cb120d3e964a66ecb2651119b4c
Connie Cheung <conniedcheung@gmail.com>
2016-04-21 15:31:31 -0700

    Move setRecurringEvent function to Twilio file

--- server/workers/twilio-api-call.js
+++ server/workers/twilio-api-call.js
@@ -1,5 +1,9 @@
 var Keys   = require('../api_keys.js');
 var Event  = require('../app/models/event.js');
+var User       = require('../app/models/user.js');
+var moment     = require('moment');
+// moment().format();
+
 //require the Twilio module and create a REST client
 var client = require('twilio')(Keys.twilioAccountSid, Keys.twilioAuthToken);
 
@@ -17,9 +21,69 @@ var displayTime = function(time) {
   return hours + ':' + minutes + ' ' + postfix;
 };
 
+var addRecurringEvent = function(event, eventTime) {
+  var userId       = event.userId;
+  var mode         = event.mode;
+  var repeat       = event.repeat;
+  var eventName    = event.eventName;
+  var address      = event.address;
+  var city         = event.city;
+  var state        = event.state;
+  var earlyArrival = event.earlyArrival;
+  var eventTime    = eventTime;
+
+  new User({ id: userId })
+    .fetch()
+    .then(function(user) {
+      var newEvent = new Event({
+        userId: user.get('id'),
+        mode: mode,
+        repeat: repeat,
+        eventName: eventName,
+        eventTime: eventTime,
+        address: address,
+        city: city,
+        state: state,
+        twilioSent: 'false',
+        earlyArrival: earlyArrival,
+      });
+      newEvent.save()
+        .then(function(createdEvent) {
+          console.log('Created new recurring event: ', createdEvent);
+          // res.status(201).send(createdEvent);
+        })
+        .catch(function(err) {
+          console.error('Could not create new recurring event 1: ', err);
+          // res.status(500).send(err);
+        });
+    })
+    .catch(function(err) {
+      console.error('Could not create new recurring event 2: ', err);
+      // res.status(500).send(err);
+    });
+};
+
+var setRecurringEventTime = function(event) {
+  var time, newEventTime;
+
+  if (event.repeat === 'Daily') {
+    time = moment(event.eventTime).add(1, 'day').format('ddd MMM D YYYY k:mm:ss');
+    newEventTime = time + ' GMT-0700 (PDT)'; 
+  } else if (event.repeat === 'Weekly') {
+    time = moment(event.eventTime).add(1, 'week').format('ddd MMM D YYYY k:mm:ss');
+    newEventTime = time + ' GMT-0700 (PDT)';
+  } else if (event.repeat === 'Monthly') {
+    time = moment(event.eventTime).add(1, 'month').format('ddd MMM D YYYY k:mm:ss');
+    newEventTime = time + ' GMT-0700 (PDT)';
+  }
+
+  addRecurringEvent(event, newEventTime);
+};
+
+
 var sendText = function(userPhoneNumber, event, timeoutTime) {
   var eventTime = displayTime(event.eventTime);
-  const { eventName, state, mode } = event;
+  const { eventName, state, mode, repeat } = event;
   const address = event.address.replace(/\s/g, '+');
   const city = event.city.replace(/\s/g, '+');
   //send twilio text
@@ -50,7 +114,13 @@ var sendText = function(userPhoneNumber, event, timeoutTime) {
       .catch(function(err) {
         console.log(err);
       });
+
+    // if event is set to repeat, re-add event to database
+    if (event.repeat !== 'Never') {
+      setRecurringEventTime(event); 
+    }
   }, time);
 }
+
 
 module.exports = sendText;
