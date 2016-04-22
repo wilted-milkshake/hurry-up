b97b408daa843c40895d31b63a02df59b351326e
Megan Smith <megan@tokaypress.com>
2016-04-22 13:48:30 -0700

    Change sign in page styling

--- client/views/signin.js
+++ client/views/signin.js
@@ -8,9 +8,11 @@ import React, {
   StyleSheet,
   AppRegistry,
   TouchableHighlight,
+  Animated
 } from 'react-native';
 
 import {createUser, login} from '../helpers/request-helpers';
+import Video from 'react-native-video';
 
 const windowSize = Dimensions.get('window');
 
@@ -25,6 +27,7 @@ class Login extends Component {
       signup: false,
       loggedIn: props.loggedIn,
       handleClick: props.handlePress,
+      fadeAnim: new Animated.Value(0)
     };
   }
 
@@ -60,107 +63,130 @@ class Login extends Component {
     this.render();
   }
 
+  goToLogin() {
+    this.setState({signup: false});
+    this.render();
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
-          <Image
-            style={styles.mark}
-            resizeMode="contain"
-            source={require('../rabbitLogo.png')}/>
-        </View>
+        <Video source={{uri:"NYC-Traffic"}}
+            style={styles.backgroundVideo}
+            paused={false}
+            rate={1} volume={1} muted={true}
+            resizeMode="cover" repeat={true}
+          />
+          <Animated.View style={{opacity: this.state.fadeAnim}}>
+            <Text style={styles.title}>Hurry Up</Text>
+          </Animated.View>
         {this.state.signup
-          ? (<View style={styles.inputs}>
-              <View style={styles.inputContainer}>
-                <Image
-                  style={styles.inputUsername}
-                  source={{uri: 'http://i.imgur.com/iVVVMRX.png'}}/>
-                <TextInput
-                  placeholder="Username"
-                  value={this.state.username}
-                  placeholderTextColor="#939393"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  style={[styles.input, styles.whiteFont]}
-                  onChangeText={(username) => this.setState({username})}/>
-              </View>
-              <View style={styles.inputContainer}>
-                <Image
-                  style={styles.inputPassword}
-                  source={{uri: 'http://i.imgur.com/ON58SIG.png'}}/>
-                <TextInput
-                  password={true}
-                  placeholder="Password"
-                  value={this.state.password}
-                  placeholderTextColor="#939393"
-                  style={[styles.input, styles.whiteFont]}
-                  onChangeText={(password) => this.setState({password})}/>
-              </View>
-              <View style={styles.inputContainer}>
-                <Image
-                  style={styles.inputPassword}
-                  source={require('../telephone.png')}/>
-                <TextInput
-                  style={[styles.input, styles.whiteFont]}
-                  placeholder="Phone Number"
-                  placeholderTextColor="#939393"
-                  keyboardType="phone-pad"
-                  value={this.state.phoneNumber}
-                  onChangeText={(phoneNumber) => this.setState({phoneNumber})}/>
-              </View>
-              <TouchableHighlight
-                style={styles.signup}
-                onPress={this.onSignup.bind(this)}>
-                  <Text style={styles.whiteFont}>
-                    Sign Up
-                  </Text>
-              </TouchableHighlight>
-            </View>)
-          :(<View style={styles.inputs}>
+        ? (<View style={styles.inputs}>
             <View style={styles.inputContainer}>
               <Image
                 style={styles.inputUsername}
-                source={{uri: 'http://i.imgur.com/iVVVMRX.png'}}/>
+                source={require('../user.png')}/>
               <TextInput
                 placeholder="Username"
                 value={this.state.username}
-                placeholderTextColor="#939393"
+                placeholderTextColor="#2C3539"
                 autoCapitalize="none"
                 autoCorrect={false}
-                style={[styles.input, styles.whiteFont]}
+                style={[styles.input, styles.greyFont]}
                 onChangeText={(username) => this.setState({username})}/>
             </View>
             <View style={styles.inputContainer}>
               <Image
                 style={styles.inputPassword}
-                source={{uri: 'http://i.imgur.com/ON58SIG.png'}}/>
+                source={require('../unlock.png')}/>
               <TextInput
                 password={true}
                 placeholder="Password"
                 value={this.state.password}
-                placeholderTextColor="#939393"
-                style={[styles.input, styles.whiteFont]}
+                placeholderTextColor="#2C3539"
+                style={[styles.input, styles.greyFont]}
                 onChangeText={(password) => this.setState({password})}/>
             </View>
-            <TouchableHighlight
-              style={styles.signin}
-              onPress={this.onLogin.bind(this)}>
-                  <Text style={styles.whiteFont}>
+            <View style={styles.inputContainer}>
+              <Image
+                style={styles.inputUsername}
+                source={require('../telephone.png')}/>
+              <TextInput
+                style={[styles.input, styles.greyFont]}
+                placeholder="Phone Number"
+                placeholderTextColor="#2C3539"
+                keyboardType="phone-pad"
+                value={this.state.phoneNumber}
+                onChangeText={(phoneNumber) => this.setState({phoneNumber})}/>
+            </View>
+            <View style={styles.border}>
+              <TouchableHighlight
+                style={styles.signin}
+                onPress={this.onSignup.bind(this)}>
+              <Text style={styles.greyFont}>
+                  Sign Up
+              </Text>
+            </TouchableHighlight>
+          </View>
+        </View>)
+        : (<View style={styles.inputs}>
+            <View style={styles.inputContainer}>
+              <Animated.View style={{opacity: this.state.fadeAnim}}>
+                <Image
+                  style={styles.inputUsername}
+                  source={require('../user.png')}/>
+                <TextInput
+                  placeholder="Username"
+                  value={this.state.username}
+                  placeholderTextColor="#2C3539"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  style={[styles.input, styles.greyFont]}
+                  onChangeText={(username) => this.setState({username})}/>
+              </Animated.View>
+            </View>
+            <View style={styles.inputContainer}>
+              <Animated.View style={{opacity: this.state.fadeAnim}}>
+                <Image
+                  style={styles.inputPassword}
+                  source={require('../unlock.png')}/>
+                <TextInput
+                  password={true}
+                  placeholder="Password"
+                  value={this.state.password}
+                  placeholderTextColor="#2C3539"
+                  style={[styles.input, styles.greyFont]}
+                  onChangeText={(password) => this.setState({password})} />
+              </Animated.View>
+            </View>
+            <View style={styles.siginBtn}>
+              <Animated.View style={{opacity: this.state.fadeAnim}}>
+                <TouchableHighlight
+                  style={styles.signin}
+                  onPress={this.onLogin.bind(this)}>
+                  <Text style={styles.greyFont}>
                     Sign In
                   </Text>
-            </TouchableHighlight>
+                </TouchableHighlight>
+              </Animated.View>
+            </View>
           </View>)
         }
-
         {this.state.signup
-          ? null
+          ? (<View style={styles.signupButton}>
+              <Text style={styles.greyFont}>Already have an account?</Text>
+              <TouchableHighlight onPress={this.goToLogin.bind(this)}>
+                <Text style={styles.whiteFont}>Login</Text>
+              </TouchableHighlight>
+            </View>)
           : (<View style={styles.signupButton}>
               <Text style={styles.greyFont}>Don't have an account?</Text>
               <TouchableHighlight onPress={this.goToSignup.bind(this)}>
                 <Text style={styles.whiteFont}>Sign Up</Text>
               </TouchableHighlight>
             </View>)
-        }
+          }
       </View>
     );
   }
@@ -171,77 +197,83 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    opacity: 0.6,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
   bg: {
-      top: 0,
-      left: 0,
-      position: 'absolute',
-      width: windowSize.width,
-      height: windowSize.height,
+    top: 0,
+    left: 0,
+    position: 'absolute',
+    width: windowSize.width,
+    height: windowSize.height,
+  },
+  title: {
+    fontSize: 40,
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    top: 150,
+    fontFamily: 'HelveticaNeue',
   },
   header: {
-      flex: .4,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'transparent'
+    flex: .4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
   },
   mark: {
-      width: 150,
-      height: 200
+    width: 150,
+    height: 200,
   },
   signin: {
-      padding: 20,
-      marginTop: 30,
-      alignItems: 'center',
-      backgroundColor: '#34778A',
-  },
-  signup: {
-      flex: 1,
-      padding: 20,
-      marginTop: 30,
-      marginBottom: 30,
-      alignItems: 'center',
-      backgroundColor: '#34778A',
+    flex: 1,
+    padding: 10,
+    margin: 30,
+    alignItems: 'center',
+    backgroundColor: '#EEE',
   },
   signupButton: {
     flex: .15,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 30
   },
   inputs: {
-      flex: .38,
-      marginTop: 10,
-      marginBottom: 10,
+    flex: .38,
+    top: 225,
   },
   inputPassword: {
-      width: 20,
-      height: 21,
-      marginLeft: 15,
+    width: 15,
+    height: 21,
+    marginLeft: 15,
   },
   inputUsername: {
-    width: 20,
-    height: 20,
+    width: 22,
+    height: 22,
     marginLeft: 15,
   },
   inputContainer: {
-      margin: 10,
-      padding: 10,
-      borderWidth: 1,
-      borderBottomColor: '#CCC',
-      borderColor: 'transparent',
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderBottomColor: '#2C3539',
+    borderColor: 'transparent',
   },
   input: {
-      top: 12,
-      right: 0,
-      left: 61,
-      height: 20,
-      position: 'absolute',
-
+    top: 12,
+    right: 0,
+    left: 61,
+    height: 20,
+    position: 'absolute',
   },
   greyFont: {
-    color: '#D8D8D8'
+    color: 'black'
   },
   whiteFont: {
     fontSize: 16,
--- index.ios.js
+++ index.ios.js
@@ -38,33 +38,25 @@ class hurryup extends Component {
   render() {
     return (
       <View style={styles.parent}>
-        <Image
-          style={styles.bg}
-          source={require('./client/background.png')}/>
-        <Text style={styles.welcome}>
-          hurryup
-        </Text>
         {this.state.loggedIn
-          ? (<ScrollableTabView
-              page={0}
-              style={{marginTop: 0, top: 0}}
-              tabBarUnderlineColor="#F5F5F6"
-              tabBarActiveTextColor="#F5F5F6"
-              tabBarInactiveTextColor="#ACB2BE"
-              tabBarBackgroundColor="transparent"
-              tabBarTextStyle={{fontFamily: 'HelveticaNeue-Light', fontSize: 15}}>
-              <CreateEvent userId = {this.state.userId} tabLabel='Create Event' />
-              <AllEvents userId = {this.state.userId} tabLabel='My Events' />
-            </ScrollableTabView>)
-          : (<ScrollableTabView
-              style={{marginTop: 0, top: 0}}
-              tabBarUnderlineColor="#F5F5F6"
-              tabBarActiveTextColor="#F5F5F6"
-              tabBarInactiveTextColor="#ACB2BE"
-              tabBarBackgroundColor="transparent"
-              tabBarTextStyle={{fontFamily: 'HelveticaNeue-Light', fontSize: 15}}>
-              <Login loggedIn = {this.state.loggedIn} handlePress = {this.handleSignIn.bind(this)} tabLabel=''/>
-            </ScrollableTabView>)
+          ? (
+            <View>
+              <Image
+                style={styles.bg}
+                source={require('./client/background.png')}/>
+              <ScrollableTabView
+                page={0}
+                style={{marginTop: 20, top: 20}}
+                tabBarUnderlineColor="#F5F5F6"
+                tabBarActiveTextColor="#F5F5F6"
+                tabBarInactiveTextColor="#ACB2BE"
+                tabBarBackgroundColor="transparent"
+                tabBarTextStyle={{fontFamily: 'HelveticaNeue-Light', fontSize: 15}}>
+                <CreateEvent userId = {this.state.userId} tabLabel='Create Event' />
+                <AllEvents userId = {this.state.userId} tabLabel='My Events' />
+              </ScrollableTabView>
+            </View>)
+          : (<Login loggedIn = {this.state.loggedIn} handlePress = {this.handleSignIn.bind(this)} tabLabel=''/>)
         }
       </View>
     );
@@ -90,13 +82,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'transparent',
   },
-  welcome: {
-    fontSize: 25,
-    marginTop: 20,
-    color: '#F5F5F6',
-    textAlign: 'center',
-    fontFamily: 'HelveticaNeue',
-  },
-});
+ });
 
 AppRegistry.registerComponent('hurryup', () => hurryup);
